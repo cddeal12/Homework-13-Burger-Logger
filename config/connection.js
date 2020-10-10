253d9029c0456d33e7f0bf7217874007c2fb06ad
Nemancer <cddeal12@gmail.com>
2020-10-09 19:28:36 -0700

    Removed postgres from connection.js

--- config/connection.js
+++ config/connection.js
@@ -1,28 +1,6 @@
 // Set up MySQL connection.
 const mysql = require("mysql");
 
-// Sets up postgres
-const {Client} = require("pg");
-const client = new Client({
-  connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false
-  }
-});
-client.connect();
-client.query(`CREATE DATABASE IF NOT EXISTS burgers_db;
-USE burgers_db;
-
-CREATE TABLE burgers (
-    id INT NOT NULL AUTO_INCREMENT,
-    PRIMARY KEY (id),
-    burger_name VARCHAR(255),
-    devoured BOOLEAN
-);`, (err, res) => {
-  if (err) throw err;
-  client.end;
-});
-
 
 var connection = mysql.createConnection({
   host: "localhost",
